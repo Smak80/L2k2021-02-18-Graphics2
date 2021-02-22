992b78e797e5b5525a324ed83ed7c7505d6f7fe5
Smak80 <smaklets@kpfu.ru>
2021-02-22 14:06:53 +0300

    Добавьте файлы проекта.

---
+++ L2k2021-02-18-Graphics2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L2k2021_02_18_Graphics2
+{
+    public partial class Form1 : Form
+    {
+        private Painter p;
+        public Form1()
+        {
+            InitializeComponent();
+            p = new Painter();
+            p.VertexCount = 12;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mainPanel.Refresh();
+            if (sender == button2)
+            {
+                var gr = Graphics.FromImage(p.Img);
+                gr.FillRectangle(new SolidBrush(Color.Black), 0, 0, 200, 200);
+                p.Img.Save(@"C:\Smak\Desktop\1.jpg");
+            }
+        }
+
+        private void mainPanel_Paint(object sender, PaintEventArgs e)
+        {
+            p.Paint(mainPanel.CreateGraphics());
+        }
+
+    }
+}
---
+++ L2k2021-02-18-Graphics2/Painter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2k2021_02_18_Graphics2
+{
+    class Painter
+    {
+        private int lineSize = 3;
+        public Color VertexColor = Color.BlueViolet;
+        private int vertexSize = 30;
+        private int vertexCount = 1;
+        //private RectangleF sr;
+        private Image img;
+        public Image Img
+        {
+            get => img;
+            private set => img = value;
+        }
+
+        public int VertexCount
+        {
+            get { return vertexCount; }
+            set { vertexCount = (value <= 0) ? 1 : value; }
+        }
+
+        private RectangleF GetSquareRect(RectangleF r)
+        {
+            var minSz = Math.Min(r.Width, r.Height) - 2 * lineSize;
+            RectangleF res = new RectangleF(
+                (r.Width - minSz) / 2,
+                (r.Height - minSz) / 2,
+                minSz,
+                minSz
+                );
+            return res;
+        }
+
+        public void Paint(Graphics g)
+        {
+            Img = new Bitmap((int)g.VisibleClipBounds.Width, (int)g.VisibleClipBounds.Height);
+            Graphics ig = Graphics.FromImage(Img);
+            ig.SmoothingMode = SmoothingMode.AntiAlias;
+            DrawVertices(ig);
+            g.Clear(Color.White);
+            g.DrawImage(Img, 0, 0);
+        }
+
+        private void DrawVertices(Graphics g)
+        {
+            var sr = GetSquareRect(g.VisibleClipBounds);
+            var center = new PointF(sr.X + sr.Width / 2, sr.Y + sr.Height / 2);
+            g.TranslateTransform(center.X, center.Y);
+            Pen p = new Pen(VertexColor, lineSize);
+            var fnt = new Font("Times New Roman", 14, FontStyle.Bold | FontStyle.Underline);
+            var b = new SolidBrush(VertexColor);
+            for (int i = 1; i <= VertexCount; i++)
+            {
+                var text_sz = g.MeasureString(i.ToString(), fnt);
+                var pos = new PointF(-text_sz.Width/2, (-sr.Height + vertexSize - text_sz.Height) / 2);
+                g.RotateTransform(360 / VertexCount);
+                g.DrawEllipse(p, -vertexSize / 2, -sr.Height / 2, vertexSize, vertexSize);
+                g.DrawString(i.ToString(), fnt, b, pos);
+            }
+            g.ResetTransform();
+            g.DrawEllipse(new Pen(Color.Black), sr.X, sr.Y, sr.Width, sr.Height);
+        }
+    }
+}
